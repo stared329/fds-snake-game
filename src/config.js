28b7bdc2e09b38d2932302e5a5d9aeb879db8de8
stared329 <jamie.kim.0329@gmail.com>
2017-12-14 17:08:22 +0900

    snakegame 로직 완성

--- src/config.js
+++ src/config.js
@@ -3,4 +3,4 @@ export const AUTO_START = true;
 export const ROWS = 15;
 export const COLS = 20;
 export const INITIAL_DELAY = 2000;
-export const DELAY_EXPONENT = 1;
+export const DELAY_EXPONENT = 0.95;
